d7f5f78fbf86811468f42de53aecff1bfc61ba55
uN1ck <qserxsdiv@gmail.com>
2014-10-06 19:17:24 +0700

    Добавлен класс cTank, отвечающий за цистерну со спайсом

--- OOP DUNE GAME/OOP DUNE GAME/cTank.cpp
+++ OOP DUNE GAME/OOP DUNE GAME/cTank.cpp
@@ -1,10 +1,53 @@
 #include "cTank.h"
 
 
-cTank::cTank()
+cTank::cTank() : cObject()
 {
+	this->setFilling(0);
+	this->setSize(10);
+}
+cTank::cTank(const cTank& value) : cObject(value)
+{
+	this->setSize(value.getSize());
+	this->setFilling(value.getFilling());
+}
+cTank::cTank(cObject *value, int in_filling, int in_size) : cObject(*value)
+{
+	this->setSize(in_size);
+	this->setFilling(in_filling);
 }
 
+void cTank::setFilling(int value)
+{
+	this->filling = ((value > this->size)? size:value);
+}
+int cTank::getFilling() const
+{
+	return this->filling;
+}
+
+void cTank::setSize(int value)
+{
+	this->size = value;
+}
+int cTank::getSize() const
+{
+	return this->size;
+}
+
+map <string, string> *cTank::readFile()
+{
+	map <string, string> * file = ((cObject*)this)->readFile();
+	this->setSize(atoi(file->find("#TankSize")->second.c_str()));
+	this->setFilling(atoi(file->find("#TankFilling")->second.c_str()));
+	return file;
+}
+
+cTank* cTank:: operator+(const cTank &value)
+{
+	this->setFilling(this->getFilling() + value.getFilling());
+	return this;
+}
 
 cTank::~cTank()
 {
--- OOP DUNE GAME/OOP DUNE GAME/cTank.h
+++ OOP DUNE GAME/OOP DUNE GAME/cTank.h
@@ -3,10 +3,37 @@
 class cTank :
 	public cObject
 {
+	//Поля
 private:	int filling;
 private:	int size;
-public:
-	cTank();
-	virtual ~cTank();
+
+	//Конструкторы
+			/*Конструктор по умолчанию <без параметров>*/
+public:		cTank();
+			/*Конструктор копирования <cTank>*/
+public:		cTank(const cTank& value);
+			/*Конструктор с параметрами <cObject, текущая наполненность, текущий размер>*/
+public:		cTank(cObject *value, int in_filling, int in_size);
+	//Деструктор
+public:		virtual ~cTank();
+
+	//Set-Get методы класса
+			/*Задать текущую наполненность*/
+public:		virtual void setFilling(int value);
+			/*Взять текущую наполненность*/
+public:		int getFilling() const;
+
+			/*Задать максимальный размер*/
+public:		virtual void setSize(int value);
+			/*Взять максимальный размер*/
+public:		int getSize() const;
+
+	//Перегрузки операторов
+			/*Перегрузка оператора сложения, цели испольхования 1) слив двух баков в один*/
+public:		cTank* operator+(const cTank &value);
+	//Перегруженные методы от наследника
+			/*Чтение параметров цистерны с файла*/
+public:		virtual map< string, string > *readFile();
+
 };
 
