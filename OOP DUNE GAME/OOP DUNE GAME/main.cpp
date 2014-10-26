54547e2d11b8a0f2c7c89472072851bd077b61b3
uN1ck <qserxsdiv@gmail.com>
2014-10-26 18:48:00 +0600

    Мелкие изменения в main.cpp

--- OOP DUNE GAME/OOP DUNE GAME/main.cpp
+++ OOP DUNE GAME/OOP DUNE GAME/main.cpp
@@ -1,10 +1,15 @@
 #include <iostream>
+#include "cBase.h"
 
 using namespace std;
 
+cBase *first, *second;
 
 int main()
 {
+	first = new cBase();
+	cout << first->toString();
+
 
 	system("pause");
 
